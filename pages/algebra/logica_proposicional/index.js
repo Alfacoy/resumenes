f30433270d3014490aa2ce2927630f3a1e8e0411
Brian Durand <briandurand1990@gmail.com>
2020-04-05 17:05:39 -0300

    Algebra/logica_proposicional terminada!

--- pages/algebra/logica_proposicional/index.js
+++ pages/algebra/logica_proposicional/index.js
@@ -32,14 +32,14 @@ const Proposicional = () => (
           todas las ciencias.
         </p>
 
-        <diV className={style.remarque}>
+        <div className={style.remarque}>
           <p>
             Una ciencia es producto de razonamientos y discursos. En este caso
             de argumentos. El nucleo de la lógica es el argumento y el argumento
             es el cimiento de las ciencias. Por consiguiente, la lógica es la
             base de todas las ciencias.
           </p>
-        </diV>
+        </div>
       </article>
 
       <article className={style.fora}>
@@ -72,16 +72,141 @@ const Proposicional = () => (
             <u>Proposiciones simples</u>
           </b>
           : son aquellas que carecen de conectores lógicos. Por ejemplo:
-          "Argentina forma parte de America del Sur".
+          <span className={style.remarque}>
+            "Argentina forma parte de America del Sur"
+          </span>
+          .
         </p>
         <p>
           <b>
             <u>Proposiciones complejas</u>
           </b>
           : son el resultado de varias proposiciones simples, unidas por uno o
-          más conectores lógicos. Por ejemplo: "Argentina forma parte de America
-          del sur y limita con cinco paises de este."
+          más conectores lógicos. Por ejemplo:{" "}
+          <span className={style.remarque}>
+            "Argentina forma parte de America del sur y limita con cinco paises
+            de este"
+          </span>
+          .
         </p>
+
+        <p>
+          Las proposiciones son representadas con letras en minúsculas. (p, q,
+          r, s, t, u, etc)
+        </p>
+
+        <h3>Conectores Lógicos</h3>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Negación</th>
+              <th>Conjunción</th>
+              <th>Disyunción</th>
+              <th>Disyunción Excluyente</th>
+              <th>Condicional</th>
+              <th>Bicondicional</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>~P</td>
+              <td>P &#8743; Q</td>
+              <td>P V Q</td>
+              <td>P &#8891; Q</td>
+              <td>P &#8594; Q</td>
+              <td>P ↔ Q</td>
+            </tr>
+            <tr>
+              <td>No P</td>
+              <td>P y Q</td>
+              <td>P o Q</td>
+              <td>P o Q no ambas</td>
+              <td>P entonces Q</td>
+              <td>P si y solo si Q</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h3>Tabla de verdad</h3>
+
+        <table>
+          <thead>
+            <tr>
+              <th>P</th>
+              <th>Q</th>
+              <th>P &#8743; Q</th>
+              <th>P V Q</th>
+              <th>P &#8891; Q</th>
+              <th>P &#8594; Q</th>
+              <th>P ↔ Q</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>1</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>0</td>
+              <td>1</td>
+              <td>0</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0</td>
+              <td>0</td>
+              <td>1</td>
+              <td>1</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+              <td>0</td>
+              <td>1</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <ul className={style.listas}>
+          <li>
+            Siempre que se compare <span className={style.remarque}>P y Q</span>
+            , ambos deben valer 1 para que su resultado de 1. Sino, es 0.
+          </li>
+          <li>
+            Siempre que se compare <span className={style.remarque}>P o Q</span>
+            , almenos uno debe valer 1 para que su resultado sea 1. Sino, es 0.
+          </li>
+          <li>
+            Siempre que se compare{" "}
+            <span className={style.remarque}>P o Q, no ambas</span>, una de las
+            dos debe valer 1 para que su resultado de 1. Si son iguales, es 0.
+          </li>
+          <li>
+            Siempre que se compare{" "}
+            <span className={style.remarque}>P entonces Q</span>, y el primer
+            valor sea 1 y el segundo 0, va a valer 0. Todos los demas valen 1.
+          </li>
+          <li>
+            Siempre que se compare{" "}
+            <span className={style.remarque}>P si y solo si Q</span>, ambos
+            deben valer igual para que su resultado de 1. Sino, es 0.
+          </li>
+        </ul>
       </article>
     </Content>
   </Layout>
