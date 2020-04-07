9d060e0bc14decafa6049ed070b20bde11200cd7
Brian Durand <briandurand1990@gmail.com>
2020-04-07 11:24:47 -0300

    Se agrega un <Head> con los meta default y se niega la indexacion.

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import Link from "next/link";
 import style from "./style.scss";
 
 const Header = () => (
   <header className={style.header}>
+    <Head>
+      <title>Resumenes</title>
+      <meta name="robots" content="noindex" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
     <Link href="/">
       <h1>Resumenes</h1>
     </Link>
